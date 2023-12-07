d04a452322c0c246752fe8891ac349253a36e7f0
argentoff1 <mparinos56@gmail.com>
2023-12-07 11:01:47 +0300

    success

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,82 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, String> services = new HashMap<>();
-        services.put("газоснабжение", "газоснабжение");
-        services.put("теплоснабжение", "теплоснабжение");
-        services.put("гвс", "гвс");
-        services.put("xвс", "xвс");
-        services.put("электроснабжение", "электроснабжение");
-
-        String[] months = {"январь", "февраль", "март", "апрель", "май", "июнь", "июль",
-                "август", "сентябрь", "октябрь", "ноябрь", "декабрь"};
+        Map<String, List<String>> data;
 
         try {
-            File file = new File("C:\\Users\\Maxim\\IdeaProjects" +
-                    "\\2023\\test-task-for-stack\\чеки.txt");
-            Scanner scanner = new Scanner(file);
-
-            HashMap<String, StringBuilder> unpaidServices = new HashMap<>();
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] parts = line.split("_");
-                String service = parts[0];
-                String month = parts[1].split(" .")[0];
-
-                if (unpaidServices.containsKey(month))
-                    unpaidServices.get(month).append("").append(line);
-                else
-                    unpaidServices.put(month, new StringBuilder(line));
-            }
-
-            File outputFile = new File("чеки_по_папкам.txt");
-            FileWriter writer = new FileWriter(outputFile);
-
-            // .pdf dobavit
-            for (String month : months) {
-                if (unpaidServices.containsKey(month + ".pdf")) {
-                    writer.write("/" + month + "/" + unpaidServices.get(month).toString() + "");
-                }
-            }
-
-            writer.write("не оплачены:");
-            for (String month : months) {
-                if (unpaidServices.containsKey(month)) {
-                    writer.write(month + ":");
-
-                    for (String key : services.keySet()) {
-                        if (!unpaidServices.get(month).toString().contains(key)) {
-                            writer.write(services.get(key) + "");
-                        }
-                    }
-                }
-            }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            data = readInputFile();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
+        try {
+            writeOutputFile(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Чтение информации о выплаченных чеках
+     *
+     * @return Данные из файла в виде коллекции Map
+     */
+    private static Map<String, List<String>> readInputFile() throws FileNotFoundException {
+        File file = new File("чеки.txt");
+        Scanner scanner = new Scanner(file);
+        Map<String, List<String>> map = new HashMap<>();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split("_");
+            String month = parts[1].split("\\.")[0];
+
+            if (!map.containsKey(month)) {
+                map.put(month, new ArrayList<>());
+            }
+            map.get(month).add(line);
+        }
+
+        return map;
+    }
+
+    /**
+     * Запись в папки информации о выплаченных чеках, а также вывод чеков,
+     * которые не были оплачены в определенных месяцах
+     *
+     * @param map Данные из исходного файла в виде коллекции Map
+     */
+    private static void writeOutputFile(Map<String, List<String>> map) throws IOException {
+        File outputFile = new File("чеки_по_папкам.txt");
+        FileWriter writer = new FileWriter(outputFile);
+
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            String month = entry.getKey();
+            writer.write("/" + month + ":\n");
+
+            for (String fileName : entry.getValue()) {
+                writer.write("/" + month + "/" + fileName + "\n");
+            }
+        }
+
+        writer.write("не оплачены:\n");
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            if (entry.getValue().size() < 12) {
+                writer.write(entry.getKey() + ":\n");
+                Set<String> services = new HashSet<>(Arrays.asList(
+                        "газоснабжение", "теплоснабжение", "гвс", "xвс", "электроснабжение"
+                ));
+                for (String fileName : entry.getValue()) {
+                    String service = fileName.split("_")[0];
+                    services.remove(service);
+                }
+                for (String service : services) {
+                    writer.write(service + "\n");
+                }
+            }
+        }
+
+        writer.close();
     }
 }
